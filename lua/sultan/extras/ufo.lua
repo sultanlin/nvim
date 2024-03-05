41225604e8dd185758022a0f93ae8b9815699021
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-03-05 04:53:43 +0300

    changing keybinds, conflict with z

--- lua/sultan/extras/ufo.lua
+++ lua/sultan/extras/ufo.lua
@@ -29,8 +29,8 @@ function M.config()
     vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
     -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-    vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-    vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+    vim.keymap.set("n", "<leader>zR", require("ufo").openAllFolds)
+    vim.keymap.set("n", "<leader>zM", require("ufo").closeAllFolds)
 
     local handler = function(virtText, lnum, endLnum, width, truncate)
         local newVirtText = {}
@@ -94,16 +94,16 @@ function M.config()
         },
     })
 
-    vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-    vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
-    vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
-    vim.keymap.set("n", "zm", require("ufo").closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
-    vim.keymap.set("n", "K", function()
-        local winid = require("ufo").peekFoldedLinesUnderCursor()
-        if not winid then
-            vim.lsp.buf.hover()
-        end
-    end)
+    vim.keymap.set("n", "<leader>zR", require("ufo").openAllFolds)
+    vim.keymap.set("n", "<leader>zM", require("ufo").closeAllFolds)
+    vim.keymap.set("n", "<leader>zr", require("ufo").openFoldsExceptKinds)
+    vim.keymap.set("n", "<leader>zm", require("ufo").closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
+    -- vim.keymap.set("n", "K", function()
+    --     local winid = require("ufo").peekFoldedLinesUnderCursor()
+    --     if not winid then
+    --         vim.lsp.buf.hover()
+    --     end
+    -- end)
 end
 
 return M
