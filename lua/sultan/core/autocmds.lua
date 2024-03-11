228404e35f98816b257c700254d164ee89f65cbb
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-03-11 03:26:34 +0300

    refactoring

--- lua/sultan/core/autocmds.lua
+++ lua/sultan/core/autocmds.lua
@@ -128,3 +128,10 @@ vim.api.nvim_create_autocmd({ "CursorHold" }, {
         end
     end,
 })
+
+-- vim.api.nvim_create_autocmd("BufReadPost", {
+--     desc = "Open file at the last position it was edited earlier",
+--     group = misc_augroup,
+--     pattern = "*",
+--     command = 'silent! normal! g`"zv',
+-- })
