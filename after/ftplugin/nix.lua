b8bb2c104f362eee91d2b302f6911c03f332da4f
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-03-16 16:04:48 +0300

    ??

--- after/ftplugin/nix.lua
+++ after/ftplugin/nix.lua
@@ -1,26 +1,29 @@
+-- FIX:
+-- https://www.reddit.com/r/neovim/comments/14pk1u5/lsp_tells_me_to_enable_autofetch_in/
 local lsp = require("sultan.lspconfig")
 
 if vim.fn.executable("nil") ~= 1 then
+    print("error")
     return
 end
 
----@diagnostic disable-next-line: missing-fields
-vim.lsp.start({
-    name = "nil_ls",
-    cmd = { "nil" },
-    root_dir = vim.fs.dirname(vim.fs.find({ "flake.nix", ".git" }, { upward = true })[1]),
-    on_attach = lsp.on_attach,
-    capabilities = lsp.capabilities,
-    settings = {
-        -- formatting = {
-        --     command = { "alejandra", "-qq" },
-        -- },
-        flake = {
-            autoArchive = true,
-            autoEvalInputs = true,
-        },
-    },
-})
+-- ---@diagnostic disable-next-line: missing-fields
+-- vim.lsp.start({
+--     name = "nil_ls",
+--     cmd = { "nil" },
+--     root_dir = vim.fs.dirname(vim.fs.find({ "flake.nix", ".git" }, { upward = true })[1]),
+--     on_attach = lsp.on_attach,
+--     capabilities = lsp.capabilities,
+--     settings = {
+--         -- formatting = {
+--         --     command = { "alejandra", "-qq" },
+--         -- },
+--         flake = {
+--             autoArchive = true,
+--             autoEvalInputs = true,
+--         },
+--     },
+-- })
 
 vim.api.nvim_set_hl(0, "@lsp.type.property.nix", {})
 vim.api.nvim_set_hl(0, "@lsp.type.variable.nix", {})
