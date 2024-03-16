b5d3f3bc2b29199f9829e56a7b77f652a3416e04
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-03-16 15:59:37 +0300

    Commented/removed unused keybinds until I understand them

--- lua/sultan/toggleterm.lua
+++ lua/sultan/toggleterm.lua
@@ -165,8 +165,8 @@ function M.config()
         bun_outdated:toggle()
     end
 
-    vim.api.nvim_set_keymap("n", "<leader>gz", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
-    vim.api.nvim_set_keymap("n", "<leader>co", "<cmd>lua _bun_outdated()<CR>", { noremap = true, silent = true })
+    -- vim.api.nvim_set_keymap("n", "<leader>gz", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+    -- vim.api.nvim_set_keymap("n", "<leader>co", "<cmd>lua _bun_outdated()<CR>", { noremap = true, silent = true })
 end
 
 return M
