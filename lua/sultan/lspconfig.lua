d5f2b90a0ec525db68c3065aa4ea1024006e1ccc
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-03-16 15:56:37 +0300

    Refactored, removed unused LSPs and fixed inlay hints
    Removed commented keymaps.
    Removed unwanted/conflicting lsps.
    Added and fixed inlay hints and added toggle inlay hints keymap.

--- lua/sultan/lspconfig.lua
+++ lua/sultan/lspconfig.lua
@@ -23,21 +23,17 @@ M.config = function()
         "clangd",
         "bashls",
         "dockerls",
-        -- "nixd",
-        -- htmx = { "html", "templ", "tmpl" },
         "gopls",
         "templ",
         "ruff_lsp",
         "tsserver",
         "html",
-        -- "rust_analyzer",
         "lua_ls",
         "omnisharp",
         "cssls",
         "jsonls",
         "yamlls",
         "marksman",
-        -- "jdtls",
         "kotlin_language_server",
     }
 
@@ -110,6 +106,12 @@ M.config = function()
 end
 
 M.on_attach = function(client, bufnr)
+    function toggle_inlay_hints()
+        if client.supports_method("textDocument/inlayHint") then
+            local ih = vim.lsp.inlay_hint
+            ih.enable(bufnr, not ih.is_enabled(bufnr))
+        end
+    end
     local bufmap = function(keys, func, descr)
         vim.keymap.set("n", keys, func, { buffer = bufnr, desc = descr, noremap = true, silent = true })
     end
@@ -143,20 +145,10 @@ M.on_attach = function(client, bufnr)
         print(vim.inspect(vim.lsp.buf.list_workspace_folder()))
     end, "[W]orkspace [L]ist folder")
 
-    -- bufmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove folder")
-
     -- bufmap("<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code [A]ction")
     bufmap("<leader>la", vim.lsp.buf.code_action, "Code [A]ction")
-    -- if client.name == "rust-analyzer" then
-    --     bufmap("<leader>la", vim.cmd.RustLsp("codeAction"), "Code [A]ction")
-    -- else
-    --     bufmap("<leader>la", vim.lsp.buf.code_action, "Code [A]ction")
-    -- end
     bufmap("<leader>li", "<cmd>LspInfo<cr>", "[I]nfo")
     bufmap("<leader>lj", "<cmd>lua vim.diagnostic.goto_next()<cr>", "Next Diagnostic")
-    -- bufmap("<leader>lh", "<cmd>lua require('sultan.lspconfig').toggle_inlay_hints()<cr>", "Toggle Inlay [H]ints")
-    -- bufmap("<leader>lh", require("sultan.lspconfig").toggle_inlay_hints(), "Toggle Inlay [H]ints")
-    -- bufmap("<leader>lh", "<cmd>lua toggle_inlay_hints()<cr>", "Toggle Inlay [H]ints")
     bufmap("<leader>lk", "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Prev Diagnostic")
     bufmap("<leader>ll", "<cmd>lua vim.lsp.codelens.run()<cr>", "Code[L]ens Action")
     bufmap("<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", "[R]ename")
@@ -172,83 +164,53 @@ M.on_attach = function(client, bufnr)
         { buffer = bufnr, desc = "Code [A]ction", noremap = true, silent = true }
     )
 
-    -- bufmap("<leader>lh", require("sultan.lspconfig").toggle_inlay_hints(client, bufnr), "Toggle Inlay [H]ints")
-    require("sultan.lspconfig").toggle_inlay_hints(client, bufnr)
-    -- vim.keymap.set(
-    --     "n",
+    -- NOTE: This is the latest working inlay hint function
+    -- Still doesn' work well (buggy when inlay hints is not supported by lsp)
+    -- bufmap(
     --     "<leader>lh",
-    --     require("sultan.lspconfig").toggle_inlay_hints(),
-    --     "force",
-    --     { noremap = true, silent = true, buffer = bufnr, desc = "Toggle Inlay [H]ints" }
+    --     -- "<cmd>require('sultan.lspconfig').toggle_inlay_hints(client, bufnr, vim.lsp.inlay_hint.is_enabled(bufnr))<cr>",
+    --     -- "<cmd>toggle_inlay_hints(client, bufnr, vim.lsp.inlay_hint.is_enabled(bufnr))<cr>",
+    --     -- "<cmd>lua vim.lsp.inlay_hint.enable(bufnr, vim.lsp.inlay_hint.is_enabled(vim.api.nvim_get_current_buf()))<cr>",
+    --     "<cmd>lua vim.lsp.inlay_hint.enable(vim.api.nvim_get_current_buf(), vim.lsp.inlay_hint.is_enabled(vim.api.nvim_get_current_buf()))<cr>",
+    --     -- "<cmd>lua vim.lsp.inlay_hint.enable(bufnr, true)<cr>",
+    --     "Toggle inlay hints"
     -- )
-    -- require("which-key").register({
-    -- 	["<leader>la"] = {
-    -- 		name = "LSP",
-    -- 		a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action", mode = "v" },
-    -- 	},
-    -- })
-
-    vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-        vim.lsp.buf.format({
-            bufnr = bufnr,
-            filter = function(client)
-                return client.name == "null-ls"
-            end,
-        })
-    end, { desc = "Format current buffer with LSP" })
+    -- bufmap("<leader>lh", require("sultan.lspconfig").toggle_inlay_hints, "Toggle Inlay [H]ints")
+    -- bufmap("<leader>lh", function()
 
     if client.supports_method("textDocument/inlayHint") then
-        vim.lsp.inlay_hint.enable(bufnr, true)
+        bufmap("<leader>lh", toggle_inlay_hints, "Toggle Inlay [H]ints")
+        vim.lsp.inlay_hint.enable(bufnr, false)
     end
-
-    -- Turn off formatting from LSP
-    -- if client.name == "tsserver" then
-    --     client.resolved_capabilities.document_formatting = false
-    -- end
-    -- Turn off formatting from LSP
-    -- if client.name == "jdt.ls" then
-    --     client.resolved_capabilities.document_formatting = false
-    --     -- client.resolved_capabilities.textDocument.completion.completionItem.snippetSupport = false
-    -- end
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
--- M.capabilities = vim.lsp.protocol.make_client_capabilities()
--- M.capabilities.textDocument.completion.completionItem.snippetSupport = true
 M.capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 --- toggle inlay hints
--- vim.g.inlay_hints_visible = false
-M.toggle_inlay_hints = function(client, bufnr)
-    if client.supports_method("textDocument/inlayHint") then
-        -- Util.toggle.inlay_hints(buffer, true)
-        local inlay_hints = function(buf, value)
-            local ih = vim.lsp.buf.inlay_hint or vim.lsp.inlay_hint
-            if type(ih) == "function" then
-                ih(buf, value)
-            elseif type(ih) == "table" and ih.enable then
-                if value == nil then
-                    value = not ih.is_enabled(buf)
-                end
-                ih.enable(buf, value)
-            end
-        end
-        inlay_hints(bufnr, true)
-    end
-
-    -- if vim.g.inlay_hints_visible then
-    --     vim.g.inlay_hints_visible = false
-    --     vim.lsp.inlay_hint(bufnr, false)
-    -- else
-    --     if client.server_capabilities.inlayHintProvider then
-    --         vim.g.inlay_hints_visible = true
-    --         vim.lsp.inlay_hint(bufnr, true)
-    --     else
-    --         print("no inlay hints available")
-    --     end
-    -- end
-    -- local bufnr = vim.api.nvim_get_current_buf()
-    -- vim.lsp.inlay_hint.enable(bufnr, not vim.lsp.inlay_hint.is_enabled(bufnr))
-end
+-- local toggle_inlay_hints = function(client, bufnr)
+--     local toggle_inlay_hints = function(client, bufnr)
+--         print("starting inlay hint")
+--         if client.supports_method("textDocument/inlayHint") then
+--             -- Util.toggle.inlay_hints(buffer, true)
+--             local inlay_hints = function(buf, toggle)
+--                 -- local ih = vim.lsp.buf.inlay_hint or vim.lsp.inlay_hint
+--                 -- if type(ih) == "function" then
+--                 --     ih(buf, toggle)
+--                 -- elseif type(ih) == "table" and ih.enable then
+--                 -- end
+--                 -- local ih = vim.lsp.inlay_hint
+--                 -- if toggle == nil then
+--                 --     toggle = not ih.is_enabled(buf)
+--                 -- end
+--                 -- ih.enable(buf, toggle)
+--                 -- vim.lsp.inlay_hint.enable(buf, toggle)
+--                 print("enabling inlay hint")
+--                 vim.lsp.inlay_hint.enable(buf, true)
+--             end
+--             inlay_hints(bufnr, true)
+--         end
+--     end
+-- end
 
 return M
