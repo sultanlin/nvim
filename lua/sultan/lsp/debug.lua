3aa1afc87a1bd69cd388c12d01837596a8109660
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-04-30 06:11:15 +0300

    add java debug config, didn't work, check it out later

--- lua/sultan/lsp/debug.lua
+++ lua/sultan/lsp/debug.lua
@@ -49,6 +49,29 @@ function M.config()
     dap.listeners.before.event_exited.dapui_config = function()
         dapui.close()
     end
+
+    -- require("dap").configurations.java = {
+    --     {
+    --         name = "Debug Launch (2GB)",
+    --         type = "java",
+    --         request = "launch",
+    --         vmArgs = "" .. "-Xmx2g ",
+    --     },
+    --     {
+    --         name = "Debug Attach (8000)",
+    --         type = "java",
+    --         request = "attach",
+    --         hostName = "127.0.0.1",
+    --         port = 8000,
+    --     },
+    --     {
+    --         name = "Debug Attach (5005)",
+    --         type = "java",
+    --         request = "attach",
+    --         hostName = "127.0.0.1",
+    --         port = 5005,
+    --     },
+    -- }
 end
 
 return M
