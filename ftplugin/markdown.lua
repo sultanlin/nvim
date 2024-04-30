e235fd6b730438733a6f9a2e1aab7e3a5cf957e6
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-04-30 06:18:20 +0300

    add markdown ftplugin file

---
+++ ftplugin/markdown.lua
@@ -0,0 +1,8 @@
+-- Markdown specific settings
+vim.opt.wrap = true -- Wrap text
+vim.opt.breakindent = true -- Match indent on line break
+vim.opt.linebreak = true -- Line break on whole words
+
+-- Spell check
+vim.opt.spelllang = "en_us"
+vim.opt.spell = true
