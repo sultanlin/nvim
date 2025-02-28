fa65061bf946adaeb5ba5105088806ea655370e8
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-02-28 17:57:35 +0300

    feature(lsp): improved javascript/typescript lsp settings

---
+++ lua/sultan/lsp/lspsettings/eslint.lua
@@ -0,0 +1,8 @@
+-- local util = require("lspconfig/util")
+return {
+--     settings = {
+--         on_init = function(client)
+--             client.config.settings.workingDirectory = { directory = client.config.root_dir }
+--         end,
+--     },
+}
