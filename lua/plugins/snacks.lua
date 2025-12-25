f5d37e34f687a034ca340455a0d965b044311969
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-12-25 16:56:49 +0300

    add snacks terminal keys (better move windows while using terminal)

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,3 +1,12 @@
+local function term_nav(dir)
+    ---@param self snacks.terminal
+    return function(self)
+        return self:is_floating() and "<c-" .. dir .. ">"
+            or vim.schedule(function()
+                vim.cmd.wincmd(dir)
+            end)
+    end
+end
 return {
     {
         "folke/snacks.nvim",
@@ -25,6 +34,16 @@ return {
             styles = {
                 notification = {
                     wo = { wrap = true }, -- Wrap notifications
+                },
+            },
+            terminal = {
+                win = {
+                    keys = {
+                        nav_h = { "<C-h>", term_nav("h"), desc = "Go to Left Window", expr = true, mode = "t" },
+                        nav_j = { "<C-j>", term_nav("j"), desc = "Go to Lower Window", expr = true, mode = "t" },
+                        nav_k = { "<C-k>", term_nav("k"), desc = "Go to Upper Window", expr = true, mode = "t" },
+                        nav_l = { "<C-l>", term_nav("l"), desc = "Go to Right Window", expr = true, mode = "t" },
+                    },
                 },
             },
         },
