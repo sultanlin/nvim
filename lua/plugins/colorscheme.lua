3f3c9f3c26f676fa07f532451f7c994275184605
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-12-25 22:11:55 +0300

    tokyonight remove custom darker background (maybe for transparency)

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -29,9 +29,9 @@ return {
 
             --- You can override specific color groups to use other groups or a hex color
             --- function will be called with a ColorScheme table
-            on_colors = function(colors)
-                colors.bg = "#14161b"
-            end,
+            -- on_colors = function(colors)
+            --     colors.bg = "#14161b"
+            -- end,
 
             --- You can override specific highlights to use other groups or a hex color
             --- function will be called with a Highlights and ColorScheme table
