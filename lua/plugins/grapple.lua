75c28d1ef4b371d20834e23f421607340966df51
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-12-25 16:56:11 +0300

    add grapple (harpoon replacement) for quick tags/marks

---
+++ lua/plugins/grapple.lua
@@ -0,0 +1,56 @@
+return {
+    {
+        "cbochs/grapple.nvim",
+        dependencies = {
+            { "nvim-tree/nvim-web-devicons" },
+        },
+        opts = {
+            scope = "git_branch",
+            icons = true,
+            quick_select = "123456789",
+            -- opts = {
+            --   menu = {
+            --     width = vim.api.nvim_win_get_width(0) - 4,
+            --   },
+            -- },
+        },
+        keys = {
+            {
+                "<leader>m",
+                function()
+                    local grapple = require("grapple")
+                    grapple.toggle()
+                    if grapple.exists() then
+                        vim.notify("Tagged file 󱡅 ")
+                    else
+                        vim.notify("Untagged file 󱡅 ")
+                    end
+                end,
+                desc = "Tag buffer (Grapple)",
+            },
+            { "<c-b>", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple next tag" },
+            { "<leader>bb", "<cmd>Grapple toggle_tags<cr>", desc = "Grapple menu" },
+            { "<leader>bk", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple next tag" },
+            { "<leader>bj", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple previous tag" },
+            { "<leader>1", "<cmd>Grapple select index=1<cr>", desc = "Grapple 1" },
+            { "<leader>2", "<cmd>Grapple select index=2<cr>", desc = "Grapple 2" },
+            { "<leader>3", "<cmd>Grapple select index=3<cr>", desc = "Grapple 3" },
+            { "<leader>4", "<cmd>Grapple select index=4<cr>", desc = "Grapple 4" },
+            { "<leader>5", "<cmd>Grapple select index=5<cr>", desc = "Grapple 5" },
+
+            -- { ";", "<cmd>Grapple toggle_tags<cr>", desc = "Toggle tags menu" },
+            -- { "<c-s>", "<cmd>Grapple toggle<cr>", desc = "Toggle tag" },
+            -- { "H", "<cmd>Grapple cycle_tags next<cr>", desc = "Go to next tag" },
+            -- { "L", "<cmd>Grapple cycle_tags prev<cr>", desc = "Go to previous tag" },
+            --         vim.api.nvim_create_autocmd({ "filetype" }, {
+            --             pattern = "harpoon",
+            --             callback = function()
+            --                 vim.cmd([[highlight link HarpoonBorder TelescopeBorder]])
+            --                 vim.cmd([[setlocal nonumber]])
+            --                 vim.cmd([[highlight HarpoonWindow guibg=#313132]])
+            --             end,
+            --         })
+        },
+        config = function() end,
+    },
+}
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -70,22 +70,6 @@ return {
                 },
             }
         end,
-        -- opts = {
-        --     -- similar to treesitter-text-objs
-        --     -- NOTE: "b = "Balanced ), ], }"," and "q = "Quote `, \", '","
-        --     -- For example, diq will delete inside nearest quotes
-        --     -- ? is user prompt. For example, vi?
-        --     n_lines = 500,
-        --     custom_textobjects = {
-        --         o = require("mini.ai").gen_spec.treesitter({
-        --             a = { "@block.outer", "@conditional.outer", "@loop.outer" },
-        --             i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-        --         }, {}),
-        --         -- f = require("mini.ai").gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
-        --         -- c = require("mini.ai").gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
-        --         -- t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
-        --     },
-        -- },
     },
     {
         "JoosepAlviste/nvim-ts-context-commentstring",
